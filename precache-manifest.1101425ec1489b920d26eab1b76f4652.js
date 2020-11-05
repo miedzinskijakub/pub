3eae48d574b94134c95011da79f37ef0c2f058ad
miedzinskijakub <47180627+miedzinskijakub@users.noreply.github.com>
2020-11-05 22:01:18 +0100

    Updates

---
+++ precache-manifest.1101425ec1489b920d26eab1b76f4652.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108777fc05fdc9ec3265bfd5ed7f0610",
+    "url": "/pub/index.html"
+  },
+  {
+    "revision": "ce15fccee3cc9ac73c3d",
+    "url": "/pub/static/css/main.8c0b96e2.chunk.css"
+  },
+  {
+    "revision": "b5552fa4a60631cac747",
+    "url": "/pub/static/js/2.4f2014f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pub/static/js/2.4f2014f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce15fccee3cc9ac73c3d",
+    "url": "/pub/static/js/main.25c270f6.chunk.js"
+  },
+  {
+    "revision": "f4c84dd03ca9a7edd304",
+    "url": "/pub/static/js/runtime-main.89e547a4.js"
+  },
+  {
+    "revision": "a11f49faf1921f3fd3e675cd1be24f72",
+    "url": "/pub/static/media/BaroQ.a11f49fa.svg"
+  },
+  {
+    "revision": "c8d7e20f08cf6955d1ff2ac0c1ceb374",
+    "url": "/pub/static/media/back2.c8d7e20f.jpg"
+  },
+  {
+    "revision": "c4701ee08c09c5c66d25fa08e7334a81",
+    "url": "/pub/static/media/desc.c4701ee0.png"
+  },
+  {
+    "revision": "35b902a39d1453b24f8f355951c0b53a",
+    "url": "/pub/static/media/drink1.35b902a3.jpg"
+  },
+  {
+    "revision": "24e4528bf544ea5b44b27de8c9c48348",
+    "url": "/pub/static/media/drink2.24e4528b.jpg"
+  },
+  {
+    "revision": "d51db76aef4b0e3e3dbd7c83b5d6eb51",
+    "url": "/pub/static/media/drink3.d51db76a.jpg"
+  }
+]);
